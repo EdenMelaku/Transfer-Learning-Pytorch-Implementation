22eb190d8a199ef24257b3b4dfcd3817d92900d9
eden melaku <edenzzzmelaku@gmail.com>
2019-02-18 17:13:44 +0300

    supports calculation of computation time

--- fine tuning/minstDataset+AlexNet.py
+++ fine tuning/minstDataset+AlexNet.py
@@ -1,10 +1,5 @@
-"""
-Finetuning Torchvision Models
-=============================
+# based on the tutorial on  https://pytorch.org/tutorials/beginner/finetuning_torchvision_models_tutorial.html
 
-**Author:** `Nathan Inkawhich <https://github.com/inkawhich>`__
-
-"""
 
 ######################################################################
 # In this tutorial we will take a deeper look at how to finetune and
@@ -41,6 +36,10 @@ Finetuning Torchvision Models
 
 from __future__ import print_function
 from __future__ import division
+
+import argparse
+import datetime
+
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -51,30 +50,12 @@ import matplotlib.pyplot as plt
 import time
 import os
 import copy
+import time
+
 
 print("PyTorch Version: ", torch.__version__)
 print("Torchvision Version: ", torchvision.__version__)
 
-
-# Top level data directory. Here we assume the format of the directory conforms
-#   to the ImageFolder structure
-data_dir = "/home/eden/Downloads/Compressed/mnist_png"
-
-# Models to choose from [resnet, alexnet, vgg, squeezenet, densenet, inception]
-model_name = "alexnet"
-
-# Number of classes in the dataset
-num_classes = 10
-
-# Batch size for training (change depending on how much memory you have)
-batch_size = 8
-
-# Number of epochs to train for
-num_epochs = 15
-
-# Flag for feature extracting. When False, we finetune the whole model,
-#   when True we only update the reshaped layer params
-feature_extract = False
 
 
 ######################################################################
@@ -160,6 +141,15 @@ def train_model(model, dataloaders, criterion, optimizer, num_epochs=25, is_ince
                 # statistics
                 running_loss += loss.item() * inputs.size(0)
                 running_corrects += torch.sum(preds == labels.data)
+                #saving the model
+
+                cwd = os.getcwd()
+                dir1 = cwd+"/model.pth"
+                dir2 = cwd+"optimizer.pth"
+                torch.save(model.state_dict(), dir1)
+                torch.save(optimizer.state_dict(), dir2)
+
+
 
             epoch_loss = running_loss / len(dataloaders[phase].dataset)
             epoch_acc = running_corrects.double() / len(dataloaders[phase].dataset)
@@ -453,11 +443,7 @@ def initialize_model(model_name, num_classes, feature_extract, use_pretrained=Tr
     return model_ft, input_size
 
 
-# Initialize the model for this run
-model_ft, input_size = initialize_model(model_name, num_classes, feature_extract, use_pretrained=True)
 
-# Print the model we just instantiated
-print(model_ft)
 
 ######################################################################
 # Load Data
@@ -471,6 +457,7 @@ print(model_ft)
 
 # Data augmentation and normalization for training
 # Just normalization for validation
+
 data_transforms = {
     'training': transforms.Compose([
         transforms.RandomResizedCrop(input_size),
@@ -486,134 +473,173 @@ data_transforms = {
     ]),
 }
 
-print("Initializing Datasets and Dataloaders...")
 
-# Create training and validation datasets
-image_datasets = {x: datasets.ImageFolder(os.path.join(data_dir, x), data_transforms[x]) for x in ['training', 'testing']}
-# Create training and validation dataloaders
-dataloaders_dict = {
-x: torch.utils.data.DataLoader(image_datasets[x], batch_size=batch_size, shuffle=True, num_workers=4) for x in
-['training', 'testing']}
 
-# Detect if we have a GPU available
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+if __name__ == "__main__":
 
-######################################################################
-# Create the Optimizer
-# --------------------
-#
-# Now that the model structure is correct, the final step for finetuning
-# and feature extracting is to create an optimizer that only updates the
-# desired parameters. Recall that after loading the pretrained model, but
-# before reshaping, if ``feature_extract=True`` we manually set all of the
-# parameter’s ``.requires_grad`` attributes to False. Then the
-# reinitialized layer’s parameters have ``.requires_grad=True`` by
-# default. So now we know that *all parameters that have
-# .requires_grad=True should be optimized.* Next, we make a list of such
-# parameters and input this list to the SGD algorithm constructor.
-#
-# To verify this, check out the printed parameters to learn. When
-# finetuning, this list should be long and include all of the model
-# parameters. However, when feature extracting this list should be short
-# and only include the weights and biases of the reshaped layers.
-#
+    now = datetime.datetime.now()
+    file=open("trainingTime.txt","w")
 
-# Send the model to GPU
-model_ft = model_ft.to(device)
+    start_time=time.clock()
+    file.write("programm started running at ---- "+now.hour+": "+now.minute+": "+now.hour )
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--dataFolder', required=True, help='Full path to model checkpoint')
+    args = parser.parse_args()
 
-# Gather the parameters to be optimized/updated in this run. If we are
-#  finetuning we will be updating all parameters. However, if we are
-#  doing feature extract method, we will only update the parameters
-#  that we have just initialized, i.e. the parameters with requires_grad
-#  is True.
-params_to_update = model_ft.parameters()
-print("Params to learn:")
-if feature_extract:
-    params_to_update = []
-    for name, param in model_ft.named_parameters():
-        if param.requires_grad == True:
-            params_to_update.append(param)
-            print("\t", name)
-else:
-    for name, param in model_ft.named_parameters():
-        if param.requires_grad == True:
-            print("\t", name)
 
-# Observe that all parameters are being optimized
-optimizer_ft = optim.SGD(params_to_update, lr=0.001, momentum=0.9)
+    # Top level data directory. Here we assume the format of the directory conforms
+    #   to the ImageFolder structure
+    data_dir = args.dataFolder
 
-######################################################################
-# Run Training and Validation Step
-# --------------------------------
-#
-# Finally, the last step is to setup the loss for the model, then run the
-# training and validation function for the set number of epochs. Notice,
-# depending on the number of epochs this step may take a while on a CPU.
-# Also, the default learning rate is not optimal for all of the models, so
-# to achieve maximum accuracy it would be necessary to tune for each model
-# separately.
-#
+    # Models to choose from [resnet, alexnet, vgg, squeezenet, densenet, inception]
+    model_name = "alexnet"
 
-# Setup the loss fxn
-criterion = nn.CrossEntropyLoss()
+    # Number of classes in the dataset
+    num_classes = 10
 
-# Train and evaluate
-model_ft, hist = train_model(model_ft, dataloaders_dict, criterion, optimizer_ft, num_epochs=num_epochs,
-                             is_inception=(model_name == "inception"))
+    # Batch size for training (change depending on how much memory you have)
+    batch_size = 8
 
-######################################################################
-# Comparison with Model Trained from Scratch
-# ------------------------------------------
-#
-# Just for fun, lets see how the model learns if we do not use transfer
-# learning. The performance of finetuning vs. feature extracting depends
-# largely on the dataset but in general both transfer learning methods
-# produce favorable results in terms of training time and overall accuracy
-# versus a model trained from scratch.
-#
+    # Number of epochs to train for
+    num_epochs = 15
 
-# Initialize the non-pretrained version of the model used for this run
-scratch_model, _ = initialize_model(model_name, num_classes, feature_extract=False, use_pretrained=False)
-scratch_model = scratch_model.to(device)
-scratch_optimizer = optim.SGD(scratch_model.parameters(), lr=0.001, momentum=0.9)
-scratch_criterion = nn.CrossEntropyLoss()
-_, scratch_hist = train_model(scratch_model, dataloaders_dict, scratch_criterion, scratch_optimizer,
-                              num_epochs=num_epochs, is_inception=(model_name == "inception"))
+    # Flag for feature extracting. When False, we finetune the whole model,
+    #   when True we only update the reshaped layer params
+    feature_extract = False
 
-# Plot the training curves of validation accuracy vs. number
-#  of training epochs for the transfer learning method and
-#  the model trained from scratch
-ohist = []
-shist = []
+    # Initialize the model for this run
+    global input_size
+    variable_init_Time=time.clock();
+    sec=(variable_init_Time-start_time)/100
+    min=sec/60
+    file.write("time elapsed for variable initializaton = "+sec + " seconds-------- or ------- "+min+"   minutes")
 
-ohist = [h.cpu().numpy() for h in hist]
-shist = [h.cpu().numpy() for h in scratch_hist]
+    model_ft,  input_size = initialize_model(model_name, num_classes, feature_extract, use_pretrained=True)
+    model_init_Time=time.clock()
+    sec = (model_init_Time - start_time) / 100
+    min = sec / 60
+    file.write("time elapsed for model initializaton = " + sec + " seconds-------- or ------- " + min + "   minutes")
+    # Print the model we just instantiated
+    print(model_ft)
+    print("Initializing Datasets and Dataloaders...")
 
-plt.title("Validation Accuracy vs. Number of Training Epochs")
-plt.xlabel("Training Epochs")
-plt.ylabel("Validation Accuracy")
-plt.plot(range(1, num_epochs + 1), ohist, label="Pretrained")
-plt.plot(range(1, num_epochs + 1), shist, label="Scratch")
-plt.ylim((0, 1.))
-plt.xticks(np.arange(1, num_epochs + 1, 1.0))
-plt.legend()
-plt.show()
+    # Create training and validation datasets
+    image_datasets = {x: datasets.ImageFolder(os.path.join(data_dir, x), data_transforms[x]) for x in
+                      ['training', 'testing']}
+    # Create training and validation dataloaders
+    dataloaders_dict = {
+        x: torch.utils.data.DataLoader(image_datasets[x], batch_size=batch_size, shuffle=True, num_workers=4) for x in
+        ['training', 'testing']}
+    data_init_Time=time.clock()
+    sec = (data_init_Time - start_time) / 100
+    min = sec / 60
+    file.write("time elapsed for data initializaton = " + sec + " seconds-------- or ------- " + min + "   minutes")
+    # Detect if we have a GPU available
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-######################################################################
-# Final Thoughts and Where to Go Next
-# -----------------------------------
-#
-# Try running some of the other models and see how good the accuracy gets.
-# Also, notice that feature extracting takes less time because in the
-# backward pass we do not have to calculate most of the gradients. There
-# are many places to go from here. You could:
-#
-# -  Run this code with a harder dataset and see some more benefits of
-#    transfer learning
-# -  Using the methods described here, use transfer learning to update a
-#    different model, perhaps in a new domain (i.e. NLP, audio, etc.)
-# -  Once you are happy with a model, you can export it as an ONNX model,
-#    or trace it using the hybrid frontend for more speed and optimization
-#    opportunities.
-#
+    ######################################################################
+    # Create the Optimizer
+    # --------------------
+    #
+    # Now that the model structure is correct, the final step for finetuning
+    # and feature extracting is to create an optimizer that only updates the
+    # desired parameters. Recall that after loading the pretrained model, but
+    # before reshaping, if ``feature_extract=True`` we manually set all of the
+    # parameter’s ``.requires_grad`` attributes to False. Then the
+    # reinitialized layer’s parameters have ``.requires_grad=True`` by
+    # default. So now we know that *all parameters that have
+    # .requires_grad=True should be optimized.* Next, we make a list of such
+    # parameters and input this list to the SGD algorithm constructor.
+    #
+    # To verify this, check out the printed parameters to learn. When
+    # finetuning, this list should be long and include all of the model
+    # parameters. However, when feature extracting this list should be short
+    # and only include the weights and biases of the reshaped layers.
+    #
 
+    # Send the model to GPU
+    model_ft = model_ft.to(device)
+
+    # Gather the parameters to be optimized/updated in this run. If we are
+    #  finetuning we will be updating all parameters. However, if we are
+    #  doing feature extract method, we will only update the parameters
+    #  that we have just initialized, i.e. the parameters with requires_grad
+    #  is True.
+    params_to_update = model_ft.parameters()
+    print("Params to learn:")
+    if feature_extract:
+        params_to_update = []
+        for name, param in model_ft.named_parameters():
+            if param.requires_grad == True:
+                params_to_update.append(param)
+                print("\t", name)
+    else:
+        for name, param in model_ft.named_parameters():
+            if param.requires_grad == True:
+                print("\t", name)
+
+    # Observe that all parameters are being optimized
+    optimizer_ft = optim.SGD(params_to_update, lr=0.001, momentum=0.9)
+
+    ######################################################################
+    # Run Training and Validation Step
+    # --------------------------------
+    #
+    # Finally, the last step is to setup the loss for the model, then run the
+    # training and validation function for the set number of epochs. Notice,
+    # depending on the number of epochs this step may take a while on a CPU.
+    # Also, the default learning rate is not optimal for all of the models, so
+    # to achieve maximum accuracy it would be necessary to tune for each model
+    # separately.
+    #
+
+    # Setup the loss fxn
+    criterion = nn.CrossEntropyLoss()
+    train_start=time.clock()
+    # Train and evaluate
+    model_ft, hist = train_model(model_ft, dataloaders_dict, criterion, optimizer_ft, num_epochs=num_epochs,
+                                 is_inception=(model_name == "inception"))
+
+    train_end = time.clock()
+    sec = (train_end - train_start) / 100
+    min = sec / 60
+    file.write("time elapsed for training  = " + sec + " seconds-------- or ------- " + min + "   minutes")
+    print(" model saved on current working directory ")
+
+    ######################################################################
+    # Comparison with Model Trained from Scratch
+    # ------------------------------------------
+    #
+    # Just for fun, lets see how the model learns if we do not use transfer
+    # learning. The performance of finetuning vs. feature extracting depends
+    # largely on the dataset but in general both transfer learning methods
+    # produce favorable results in terms of training time and overall accuracy
+    # versus a model trained from scratch.
+    #
+
+    # Initialize the non-pretrained version of the model used for this run
+    scratch_model, _ = initialize_model(model_name, num_classes, feature_extract=False, use_pretrained=False)
+    scratch_model = scratch_model.to(device)
+    scratch_optimizer = optim.SGD(scratch_model.parameters(), lr=0.001, momentum=0.9)
+    scratch_criterion = nn.CrossEntropyLoss()
+    _, scratch_hist = train_model(scratch_model, dataloaders_dict, scratch_criterion, scratch_optimizer,
+                                  num_epochs=num_epochs, is_inception=(model_name == "inception"))
+
+    # Plot the training curves of validation accuracy vs. number
+    #  of training epochs for the transfer learning method and
+    #  the model trained from scratch
+    ohist = []
+    shist = []
+
+    ohist = [h.cpu().numpy() for h in hist]
+    shist = [h.cpu().numpy() for h in scratch_hist]
+
+    plt.title("Validation Accuracy vs. Number of Training Epochs")
+    plt.xlabel("Training Epochs")
+    plt.ylabel("Validation Accuracy")
+    plt.plot(range(1, num_epochs + 1), ohist, label="Pretrained")
+    plt.plot(range(1, num_epochs + 1), shist, label="Scratch")
+    plt.ylim((0, 1.))
+    plt.xticks(np.arange(1, num_epochs + 1, 1.0))
+    plt.legend()
+    plt.show()
