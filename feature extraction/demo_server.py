a01b081fab116d6738d7fec7a169864a40824bd4
EDEN MELAKU <edenzzzmelaku@gmail.com>
2019-02-26 16:11:16 +0300

    base64 encoding error fixed

--- feature extraction/demo_server.py
+++ feature extraction/demo_server.py
@@ -1,21 +1,16 @@
 import base64
-import io
 
 import cv2
-
+import numpy as np
 import torch
 import torch.nn as nn
 from PIL import Image
-import numpy as np
-from cv2.cv2 import UMat
 from flask import Flask, request, make_response
 from torchvision import transforms
 
 app = Flask(__name__)
 
-
-num_classes=10
-
+num_classes = 10
 
 
 class AlexNet(nn.Module):
@@ -55,45 +50,27 @@ class AlexNet(nn.Module):
         x = self.classifier(x)
         return x
 
+
 @app.route('/')
 def index():
     print("got request ")
     a = request.args.get('image')
 
-    #image_data = request.REQUEST["image_data"].decode("base64") # The data image
+    # image_data = request.REQUEST["image_data"].decode("base64") # The data image
     print(a)
-    #imgdata = base64.b64decode(a)
-    #b64_string = a.encode().decode()
-    #image = np.fromstring(a, np.uint8)
-    # reconstruct image as an numpy array
+    print()
+    c = a.strip()
+    print(base64.urlsafe_b64decode(c))
+    with open('number.jpeg', "wb") as f:
+        f.write(base64.urlsafe_b64decode(a))
 
-    #cv2.imshow('image ',image)
-
-
-#    image = Image.open(io.BytesIO(a))
-    #image = cv2.imread(image)
     response = make_response()
     response.content_type = 'text'
-#    byte=bytearray(a)
-   # data_mat(a, true)
-   #mat = np.asarray(a, dtype=np.uint8)
-   #image = np.fromarray(mat, np.uint8)
 
-    #image=cv2.imdecode(a,1)
 
-    by=bytearray(a,'utf-8')
-    by.extend(map(ord,a ))
-    imgd=base64.b64decode(a.encode('base64'))
-    imgdata =base64.b64encode(by)
-#    imaged=a.decode('base64')
-    #nparr = np.fromstring(a.encode('base64').decode('base64'), np.uint8)
-    #img = cv2.imdecode(nparr, cv2.IMREAD_ANYCOLOR)
-    #cv2.imwrite("/home/eden/Pictures/mod/imag.jpeg", img)
-
-    fh = open("/home/eden/Pictures/mod/Saved.jpeg", "wb")
-    fh.write(imgdata)
+    fh = open("saved.jpeg", "wb")
+    fh.write(base64.urlsafe_b64decode(a))
     fh.close()
-
 
     PATH = "/home/eden/newMODEL/modelAlexNet.pth"
     device = torch.device("cpu")
@@ -106,18 +83,6 @@ def index():
     net.eval()  # Set model to evaluate mode
     print("model set to eval mode")
 
-    # model = AlexNet()
-    # optimizer= torch.optim.SGD (model.parameters(), lr=0.001, momentum=0.9)
-
-    # checkpoint = torch.load(PATH)
-    # model.load_state_dict(checkpoint['model_state_dict'])
-    # optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
-    # epoch = checkpoint['epoch']
-    # loss = checkpoint['loss']
-
-    # model.eval()
-
-    input_size = 224
 
     data_transforms = {transforms.Compose({
         transforms.Resize(224),
@@ -133,11 +98,10 @@ def index():
         transforms.CenterCrop(224), transforms.ToTensor(),
         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
     ])
-    # Create training and validation datasets
-    pat = "/home/eden/Pictures/mod/imageToSave.jpeg"
+    pat = "saved.jpeg"
     image = cv2.imread(pat)
     im = cv2.resize(image, (28, 28))
-    i = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    i = cv2.cvtColor(im, cv2.COLOR_BGR2HSV)
     LOW = np.array([0, 0, 0])
     UP = np.array([255, 150, 150])
     mask = cv2.inRange(i, LOW, UP)
@@ -146,17 +110,20 @@ def index():
     image = Image.open(pat)
     im = tn(image)
     imm = im.unsqueeze(0)
+    inp = Image.open(pat)
 
+    transform = transforms.Compose(data_transforms)
 
     outputs = net(imm)
-
+    # outputs=net(process_image(pat))
     _, preds = torch.max(outputs, 1)
-    print(preds)
-    response=preds.item()
+    print(preds.item())
+    response = str(preds.item())
+
     return response
 
 
 if __name__ == '__main__':
+    app.run(host='0.0.0.0')
 
-   app.run(host='0.0.0.0')
 
